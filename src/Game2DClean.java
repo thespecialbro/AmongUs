5228f225ddb3c5380f87ea09ec2eb6f4f5b47cc5
Sam Spence <thespecialbro@gmail.com>
2023-02-22 09:09:58 +0100

    fix tab size

--- src/Game2DClean.java
+++ src/Game2DClean.java
@@ -27,76 +27,78 @@ import javafx.scene.input.KeyEvent;
  */
 
 public class Game2DClean extends Application {
-   // Window attributes
-   private Stage stage;
-   private Scene scene;
-   private StackPane root;
+    // Window attributes
+    private Stage stage;
+    private Scene scene;
+    private StackPane root;
 
-   private static String[] args;
+    private static String[] args;
 
-   private final static String CREWMATE_IMAGE = "amongus.png"; // file with icon for a crewmate
-   private final static String CREWMATE_RUNNERS = "amongusRunners.png"; // file with icon for crewmates
-   private static final String BACKGROUND_IMAGE = "background.jpg"; // 
+    private final static String CREWMATE_IMAGE = "amongus.png"; // file with icon for a crewmate
+    private final static String CREWMATE_RUNNERS = "amongusRunners.png"; // file with icon for crewmates
+    private static final String BACKGROUND_IMAGE = "background.jpg"; //
 
-   // main program
-   public static void main(String[] _args) {
-      args = _args;
-      launch(args);
-   }
+    // main program
+    public static void main(String[] _args) {
+        args = _args;
+        launch(args);
+    }
 
-   // start() method, called via launch
-   public void start(Stage _stage) {
-      // stage seteup
-      stage = _stage;
-      stage.setTitle("Game2D Starter");
-      stage.setOnCloseRequest(
-            new EventHandler<WindowEvent>() {
-               public void handle(WindowEvent evt) {
-                  System.exit(0);
-               }
-            });
+    // start() method, called via launch
+    public void start(Stage _stage) {
+        // stage seteup
+        stage = _stage;
+        stage.setTitle("Game2D Starter");
+        stage.setOnCloseRequest(
+                new EventHandler<WindowEvent>() {
+                    public void handle(WindowEvent evt) {
+                        System.exit(0);
+                    }
+                });
 
-      // root pane
-      root = new StackPane();
+        // root pane
+        root = new StackPane();
 
-      initializeScene();
+        initializeScene();
 
-   }
+    }
 
-   // start the game scene
-   public void initializeScene() {
+    // start the game scene
+    public void initializeScene() {
 
-      // display the window
-      scene = new Scene(root, 800, 500);
-      // scene.getStylesheets().addAll(this.getClass().getResource("style.css").toExternalForm());
-      stage.setScene(scene);
-      stage.show();
-   }
+        // display the window
+        scene = new Scene(root, 800, 500);
+        // scene.getStylesheets().addAll(this.getClass().getResource("style.css").toExternalForm());
+        stage.setScene(scene);
+        stage.show();
+    }
 
-   class Crewmate extends Pane {
-      private int posX = 0;
-      private int posY = 0;
-      private ImageView aPicView = null;
+    class Crewmate extends Pane {
+        private int posX = 0;
+        private int posY = 0;
+        private ImageView aPicView = null;
 
-      public Crewmate() {
-         aPicView = new ImageView(CREWMATE_IMAGE);
-         this.getChildren().add(aPicView);
-      }
+        public Crewmate() {
+            aPicView = new ImageView(CREWMATE_IMAGE);
+            this.getChildren().add(aPicView);
+        }
 
-      public void update() {
-         double speed = 5;
+        public void update() {
+            double speed = 5;
 
-         posX += (Math.random() - 0.5) * speed;
-         posY += (Math.random() - 0.5) * speed;
+            posX += (Math.random() - 0.5) * speed;
+            posY += (Math.random() - 0.5) * speed;
 
-         // set image pos
-         this.aPicView.setTranslateX(posX);
-         this.aPicView.setTranslateY(posY);
+            // set image pos
+            this.aPicView.setTranslateX(posX);
+            this.aPicView.setTranslateY(posY);
 
-         // loop at screen edges
-         if(posX > 800) posX = 0;
-         if(posY > 500) posY = 0;
-      }
-   }
+            // loop at screen edges
+            if (posX > 800)
+                posX = 0;
+            if (posY > 500)
+                posY = 0;
+        }
+    }
 
 } // end class Races
